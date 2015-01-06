0ea671537783817c421c8699bc8db738fc0dcfe4
Eric Hagman <emhagman@gmail.com>
2015-01-06 14:53:33 -0500

    new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[:2] < (2, 6):
 
 setup(
     name='blade-flask-peewee',
-    version='0.6.8',
+    version='0.6.9',
     url='https://github.com/bladenet/flask-peewee',
     license='BSD',
     author='Charles Leifer',
